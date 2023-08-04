b93545a225de7f154dddda20b4edc41419b9414f
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-04 17:22:37 -0400

    sneakerservice test done

--- src/main/java/services/SneakerService.java
+++ src/main/java/services/SneakerService.java
@@ -1,4 +1,20 @@
 package services;
 
+import models.Sneakers;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SneakerService {
+
+    private static int nextId = 1;
+    private List<Sneakers> inventory = new ArrayList<Sneakers>();
+
+    public Sneakers create(String name, String brand, String sport, float size, int qty, float price) {
+        Sneakers newSneakers = new Sneakers(nextId++, name, brand, sport, size, qty, price);
+        return newSneakers;
+    }
+
+
+
 }
