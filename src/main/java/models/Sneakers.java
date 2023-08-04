734ece83373aac746ed8a78d836a9152f6c9d811
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-04 16:31:08 -0400

    sneakers properties

--- src/main/java/models/Sneakers.java
+++ src/main/java/models/Sneakers.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Sneakers {
+
+    private int id;
+    private String name;
+    private String brand;
+    private  String sport;
+    private float size;
+    private int qty;
+    private float price;
 }
