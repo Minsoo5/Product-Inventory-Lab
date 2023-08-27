0bff2c18a0ff336a194c691dc53d003a52bfd687
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-27 11:10:47 -0400

    App checkpoint

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,11 @@
 import io.Console;
 import services.SneakerService;
 
+import java.util.Scanner;
+
 public class App {
     private SneakerService sneakerService = new SneakerService(); // (1)
-
+    Scanner input = new Scanner(System.in);
     public static void main(String... args){
         App application = new App(); // (2)
         application.init();  // (3)
@@ -14,5 +16,7 @@ public class App {
         // application logic here
         // call methods to take user input and interface with services
         Console.printWelcome();
+        Console.mainMenu();
+        Console.mainMenuLogic(input.nextInt());
     }
 }
--- src/main/java/io/Console.java
+++ src/main/java/io/Console.java
@@ -3,13 +3,64 @@ package io;
 public class Console {
     public static void printWelcome(){
         System.out.println("" +
-                "**************************************************" +
-                "***           Welcome and Bienvenue            ***" +
-                "***                    to                      ***" +
-                "***          ZipCo Inventory Manager           ***" +
-                "**************************************************");
+                "**************************************************\n" +
+                "***           Welcome and Bienvenue            ***\n" +
+                "***                    to                      ***\n" +
+                "***          ZipCo Inventory Manager           ***\n" +
+                "**************************************************\n");
     }
 
+    public static void mainMenu() {
+        System.out.println("" +
+                "****************************************************\n" +
+                "***           Please make a selection            ***\n" +
+                "***        1  Show All Products                  ***\n" +
+                "***        2  Show Certain Product               ***\n" +
+                "***        3  Add Product                        ***\n" +
+                "***        4  Update Product                     ***\n" +
+                "***        5  Delete Product                     ***\n" +
+                "***        6  Exit                               ***\n" +
+                "***                                              ***\n" +
+                "****************************************************\n");
+    }
+
+    public static void mainMenuLogic(int selection) {
+
+        switch (selection){
+            case 1:
+                showAllProducts();
+                break;
+            case 2:
+                showCertainProduct();
+                break;
+            case 3:
+                addProduct();
+                break;
+            case 4:
+                updateProduct();
+                break;
+            case 5:
+                deleteProduct();
+                break;
+            case 6:
+                exitApp();
+        }
+
+        mainMenu();
+
+    }
+
+    public static void showAllProducts() {}
+
+    public static void showCertainProduct() {}
+
+    public static void addProduct() {}
+
+    public static void updateProduct() {}
+
+    public static void deleteProduct() {}
+
+    public static void exitApp(){}
 
 
 }
